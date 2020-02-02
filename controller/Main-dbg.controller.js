42dc9fef37dce24afe89e4bc6b5f281146548d37
SAPMarco <SAPMarco@users.noreply.github.com>
2020-02-02 21:15:59 +0000

    PersonalPage Upd. SAPMarco/github_pages@1f087d058242420a127c653d9c77a6e8f2638ae4

--- controller/Main-dbg.controller.js
+++ controller/Main-dbg.controller.js
@@ -2,14 +2,22 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller"
 ], function(Controller) {
   "use strict";
+  //declare some globals for the themes
+  var sFiori3DarkTheme = "sap_fiori_3_dark";
+  var sSapBelize       = "sap_belize"
   return Controller.extend("sapmarco.projectpages.controller.Main", {
     onInit: function(){
+      this._setInvertedStyleOnSocials = function(){
+        return this.byId("socialsGrouped").aCustomStyleClasses.indexOf("invertSocials") > -1 ? this.byId("socialsGrouped").removeStyleClass('invertSocials') : this.byId("socialsGrouped").addStyleClass("invertSocials");
+      };
       //set content density
       this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+      
       //check users prefered color scheme
       if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        sap.ui.getCore().applyTheme("sap_fiori_3_dark");
+        sap.ui.getCore().applyTheme(sFiori3DarkTheme);
         this.byId("cVRow").removeStyleClass("cV");
+        this._setInvertedStyleOnSocials();
       };
     },
     onUI5IconPress : function () {
@@ -17,13 +25,15 @@ sap.ui.define([
     },
     onThemeSwap: function(sTheme){
       //read current theme sap_belize
-      if (sap.ui.getCore().getConfiguration().getTheme() === "sap_belize"){
+      if (sap.ui.getCore().getConfiguration().getTheme() === sSapBelize){
         sap.ui.getCore().applyTheme(sTheme);
-       this.byId("cVRow").removeStyleClass("cV");
+        this.byId("cVRow").removeStyleClass("cV");
+        this._setInvertedStyleOnSocials();
       } else {
-        sap.ui.getCore().applyTheme("sap_belize");
-        this.byId("cVRow").addStyleClass("cV");
+        sap.ui.getCore().applyTheme(sSapBelize);
+        this.byId("cVRow").addStyleClass("cV");        
+        this._setInvertedStyleOnSocials();
       }
-    }
+    },
   });
 });
