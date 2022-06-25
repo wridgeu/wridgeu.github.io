de3204ec83767c0601ce305392b6dfbe9828cf0e
wridgeu <wridgeu@users.noreply.github.com>
2022-06-25 19:42:25 +0000

    PersonalPage upd. wridgeu/github_pages@dd130f44416c831c4a1b74ff4a05f4748ff91bec

---
+++ resources/highlight.js/lib/languages/json.js
@@ -0,0 +1,56 @@
+sap.ui.define((function () { 'use strict';
+
+    (function() {
+        const env = {"NODE_ENV":"production"};
+        try {
+            if (process) {
+                process.env = Object.assign({}, process.env);
+                Object.assign(process.env, env);
+                return;
+            }
+        } catch (e) {} // avoid ReferenceError: process is not defined
+        globalThis.process = { env:env };
+    })();
+
+    function json(hljs) {
+      const ATTRIBUTE = {
+        className: 'attr',
+        begin: /"(\\.|[^\\"\r\n])*"(?=\s*:)/,
+        relevance: 1.01
+      };
+      const PUNCTUATION = {
+        match: /[{}[\],:]/,
+        className: "punctuation",
+        relevance: 0
+      };
+      // normally we would rely on `keywords` for this but using a mode here allows us
+      // to use the very tight `illegal: \S` rule later to flag any other character
+      // as illegal indicating that despite looking like JSON we do not truly have
+      // JSON and thus improve false-positively greatly since JSON will try and claim
+      // all sorts of JSON looking stuff
+      const LITERALS = { beginKeywords: [
+        "true",
+        "false",
+        "null"
+      ].join(" ") };
+
+      return {
+        name: 'JSON',
+        contains: [
+          ATTRIBUTE,
+          PUNCTUATION,
+          hljs.QUOTE_STRING_MODE,
+          LITERALS,
+          hljs.C_NUMBER_MODE,
+          hljs.C_LINE_COMMENT_MODE,
+          hljs.C_BLOCK_COMMENT_MODE
+        ],
+        illegal: '\\S'
+      };
+    }
+
+    var json_1 = json;
+
+    return json_1;
+
+}));
--- resources/sap/ui/model/_Helper.js
+++ resources/sap/ui/model/_Helper.js
@@ -3,4 +3,4 @@
  * (c) Copyright 2009-2022 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
-sap.ui.define(["sap/base/util/extend","sap/base/util/merge"],function(e,s){"use strict";var t={extend:e,merge:s};return t},false);
+sap.ui.define(["sap/base/util/deepEqual","sap/base/util/extend","sap/base/util/merge"],function(e,a,s){"use strict";var t={deepEqual:e,extend:a,merge:s};return t},false);
