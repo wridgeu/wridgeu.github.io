d492fd6cc4d3241ea56e419eca3901d3998a6410
wridgeu <wridgeu@users.noreply.github.com>
2023-01-01 11:28:11 +0000

    PersonalPage upd. wridgeu/github_pages@9d2f1fdfa9f239632925e2df0a1ea23344ff49a0

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -78,7 +78,7 @@ sap.ui.define([
 		 */
 		version: "1.109.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20221223-2335" }
+		buildinfo : { lastchange : "", buildtime : "20230101-1126" }
 	});
 
 	var syncCallBehavior = Configuration.getSyncCallBehavior();
